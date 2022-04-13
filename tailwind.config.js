bfdffe613cd2fceb7187d3241f1b27b7958a5f71
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-04-13 19:46:14 -0300

    refactor: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/pages/*.{html,tsx}", "./src/components/*.{html,tsx}"],
   theme: {
     extend: {
       colors: {
