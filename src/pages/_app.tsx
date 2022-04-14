3047e539b2ff320acce444586d40d2ec3011c17a
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-04-14 16:43:13 -0300

    refactor: update components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import "../styles/globals.css";
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function GummyNotes({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default GummyNotes;
