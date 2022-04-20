b408e3814561c5d4bb385e17fb57afb7268c26ed
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-04-20 19:53:44 -0300

    refactor: update home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,14 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import LoginDialog from './login/login.dialog';
+import NavigationBar from '../components/navigation.bar';
 
 export default function HomePage() {
-  const [isLoginDialogOpened, setIsLoginDialogOpened] = useState(true);
-
-  useEffect(() => {
-    window.onclick = (e) => {
-      if (e.target && e.target.id === 'dialog-backdrop') {
-        setIsLoginDialogOpened(false);
-      } else setIsLoginDialogOpened(true);
-    };
-  }, []);
-
   return (
     <div
       className={`justify-center items-center w-full min-h-screen bg-gm-light-pink dark:bg-gm-light-purple`}>
       <Head>
         <title>Gummy Notes</title>
       </Head>
-      <h1>BoardPage</h1>
-      {isLoginDialogOpened && <LoginDialog />}
+      <NavigationBar />
     </div>
   );
 }
