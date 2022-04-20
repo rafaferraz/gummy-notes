b028f0ea5df63392f0cc745d92868f94346ff9fc
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-04-20 19:51:50 -0300

    feat: add logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <div className="flex flex-row justify-evenly items-center">
+      <span className="text-white font-extrabold tracking-widest text-xl text-center">
+        Gummy Notes
+      </span>
+    </div>
+  );
+}
