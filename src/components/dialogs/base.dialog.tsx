1535ea82d86db20df47392d0e650270123ad8174
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-04-20 19:52:51 -0300

    feat: add base dialog component

--- src/components/dialogs/base.dialog.tsx
+++ src/components/dialogs/base.dialog.tsx
@@ -1,14 +1,30 @@
+import { useEffect } from 'react';
+
 type DialogProps = {
   title: string;
+  onClose: () => void;
   children: React.ReactNode;
   footer?: React.ReactNode;
 };
 
-export default function BaseDialog({ title, children, footer }: DialogProps) {
+export default function BaseDialog({ title, onClose, children, footer }: DialogProps) {
+  function _handleDialogClose() {
+    const dialog = document.getElementById('dialog-backdrop');
+    window.addEventListener('click', (event) => {
+      if (event.target === dialog) {
+        onClose();
+      }
+    });
+  }
+
+  useEffect(() => {
+    _handleDialogClose();
+  });
+
   return (
     <div id="dialog-backdrop" className={`fixed block inset-0 backdrop-blur-sm h-full w-full`}>
       <div
-        className={`relative mx-5 my-12 max-w-[324px] lg:w-full px-6 py-8 shadow-lg bg-gm-pink dark:bg-gm-dark-purple rounded-[2rem]`}>
+        className={`relative object-center max-w-[324px] lg:w-full px-6 py-8 shadow-lg bg-gm-pink dark:bg-gm-dark-purple rounded-[2rem]`}>
         <h1 className={`text-white tracking-wider text-3xl text-center font-semibold`}>{title}</h1>
         {children}
         {footer}
