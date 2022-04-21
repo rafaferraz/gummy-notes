77617f3dc8307e893cf5af0581e05b72454865b6
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-04-21 17:53:25 -0300

    refactor: update dark mode switch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 import NavigationBar from '../components/navigation.bar';
 
 export default function HomePage() {
+  useEffect(() => {
+    const darkIsOnStorage = localStorage.getItem('dark') != null;
+    let isDarkModeActive = false;
+    if (darkIsOnStorage) isDarkModeActive = localStorage.getItem('dark') == 'true';
+    else localStorage.setItem('dark', 'false');
+    document.documentElement.classList.toggle('dark', isDarkModeActive);
+  });
+
   return (
     <div
       className={`justify-center items-center w-full min-h-screen bg-gm-light-pink dark:bg-gm-light-purple`}>
