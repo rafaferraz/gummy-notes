ce096b8bf51c2f68b9eab5ea889408d0ac70817e
Rafaela Cristina Ferraz <rafaelacristina@gec.inatel.br>
2022-05-28 16:59:54 -0300

    fix: modal z-index adjust

--- src/components/dialogs/base.dialog.tsx
+++ src/components/dialogs/base.dialog.tsx
@@ -22,7 +22,7 @@ export default function BaseDialog({ title, onClose, children, footer }: DialogP
   });
 
   return (
-    <div id="dialog-backdrop" className={`fixed flex inset-0 backdrop-blur-sm h-full w-full`}>
+    <div id="dialog-backdrop" className={`z-50 fixed flex inset-0 backdrop-blur-sm h-full w-full`}>
       <div
         className={`m-auto relative object-center max-w-[324px] lg:w-full px-6 py-8 shadow-lg bg-gm-pink dark:bg-gm-dark-purple rounded-[2rem]`}>
         <h1 className={`text-white tracking-wider text-3xl text-center font-semibold`}>{title}</h1>
