b28e8100430e47430458d02a6c1ad80469439b1a
Rafaela Cristina Ferraz <rafaelacristina@gec.inatel.br>
2022-05-28 17:02:38 -0300

    feature: add drawer button to left bar

--- src/components/left.drawer.tsx
+++ src/components/left.drawer.tsx
@@ -17,29 +17,33 @@ export default function LeftDrawer() {
   };
 
   return (
-    <nav className={`w-80 h-screen bg-gm-light-pink dark:bg-gm-purple`}>
-      <h1 className={`py-10 text-center font-extrabold text-5xl text-gm-dark-pink dark:text-white`}>
-        Quadros
-      </h1>
-      <div className={`flex flex-col gap-5 text-2xl px-5`}>
-        {boards.map((board, index) => (
-          <input
-            key={index}
-            className={`text-gm-lm-grey dark:text-gm-dark-blue font-extrabold bg-gm-light-pink dark:bg-gm-purple `}
-            name={'board'}
-            defaultValue={`${board.name} (${board.id})`}
-            id={`board-${index + 1}`}
-            onChange={(e) => handleChangeBoard(e, index)}></input>
-        ))}
-
-        {/* <p className={`text-gm-lm-grey dark:text-gm-dark-blue`}>[Título do quadro]</p>
-        <p className={`text-gm-darkest-pink dark:text-white`}>[Título do quadro atual]</p> */}
-        <button
-          className={`text-left text-gm-lm-purple dark:text-black font-extrabold`}
-          onClick={addBoard}>
-          + Novo quadro
-        </button>
+    <>
+      <div className="hidden md:flex flex-row">
+        <nav className={`w-80 h-screen bg-gm-light-pink dark:bg-gm-purple`}>
+          <h1 className={`py-10 text-center font-extrabold text-5xl text-gm-dark-pink dark:text-white`}>
+            Quadros
+          </h1>
+          <div className={`flex flex-col gap-5 text-2xl px-5`}>
+            {boards.map((board, index) => (
+              <input
+                key={index}
+                className={`focus:text-gm-darkest-pink focus:dark:text-white focus:outline-none text-gm-lm-grey dark:text-gm-dark-blue font-extrabold bg-gm-light-pink dark:bg-gm-purple `}
+                name={'board'}
+                defaultValue={`${board.name} (${board.id})`}
+                id={`board-${index + 1}`}
+                onChange={(e) => handleChangeBoard(e, index)}></input>
+            ))}
+            <button
+              className={`w-80 text-left text-gm-lm-purple dark:text-black font-extrabold`}
+              onClick={addBoard}>
+              + Novo quadro
+            </button>
+          </div>
+        </nav>
       </div>
-    </nav>
+      <div className="block md:hidden">
+        <DrawerButton onClick={() => setIsDrawerOpened(!isDrawerOpened)} />
+      </div>
+    </>
   );
 }
