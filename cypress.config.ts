b1c9b2bbdec7c11856395154734e6155b7366598
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-06-03 08:48:10 -0300

    chore: add cypress mochawesome reporter

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  reporter: 'mochawesome',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
+    }
+  }
 });
