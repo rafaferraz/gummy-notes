3eea52754ee646a88784b705e5e2e836a7f028f8
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-06-08 20:03:21 -0300

    feat: add auth context

---
+++ src/contexts/auth.context.tsx
@@ -0,0 +1,50 @@
+import { createContext, ReactNode } from 'react';
+import DependenciesContainer from '../dependencies.container';
+
+type AuthContextData = {
+  isAlreadyAuthenticated: () => Promise<boolean>;
+  auth: ({ email, password }: { email: string; password: string }) => Promise<boolean>;
+  signOut: () => Promise<boolean>;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext<AuthContextData>({
+  isAlreadyAuthenticated: async () => false,
+  auth: async () => false,
+  signOut: async () => false
+});
+
+export default function AuthProvider({ children }: AuthProviderProps) {
+  const authRepository = DependenciesContainer.instance.authRepository;
+
+  async function isAlreadyAuthenticated() {
+    const token = await authRepository.readToken();
+    if (token) {
+      if (!token.isValid) {
+        const refreshedToken = await refreshAuth();
+        return refreshedToken.isValid;
+      }
+    }
+  }
+
+  async function signOut() {
+    return await authRepository.logout();
+  }
+
+  async function auth({ email, password }: { email: string; password: string }) {
+    return await authRepository.login({ email, password });
+  }
+
+  async function refreshAuth() {
+    return await authRepository.refreshToken();
+  }
+
+  return (
+    <AuthContext.Provider value={{ isAlreadyAuthenticated, auth, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
